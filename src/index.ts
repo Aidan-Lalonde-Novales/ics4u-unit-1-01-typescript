f321e3a5aba141770c1a8ea3dec88f1f99671ead
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-09-14 13:18:25 +0000

    TypeScript Files

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * This program converts length
+ *   to weight in a 1100kg truck.
+ *
+ * By:      Aidan Lalonde-Novales
+ * Version: 1.0
+ * Since:   2022-09-13
+ */
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+
+const logLengthString = prompt('How long are your logs (m)? ')
+const logLength = parseFloat(logLengthString)
+const truckCapacity = 1100 / 20 / logLength
+
+console.log(
+  `You can fit ${truckCapacity} logs in a truck with a 1100kg capacity.`
+)
+
+console.log('\nDone.')
